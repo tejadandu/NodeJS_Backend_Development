7e6235c251a940f9cbd5a334eb315f6ed996c3a1
Teja Dandu <teja.dandu484@gmail.com>
2022-09-21 16:55:08 +0530

    added unaryoperators

---
+++ IntroductionJS-2/unaryoperators.js
@@ -0,0 +1,25 @@
+// postfix increment
+
+let x = 10;
+let c = x++;
+let y = x++;
+console.log(x, y, c);
+
+// prefix increment
+
+let p = 10;
+let d = ++p;
+let r = ++p;
+console.log(p, r, d);
+
+// postfix decrement
+let s = 10;
+let e = s--;
+let t = s--;
+console.log(s, t, e);
+
+// prefix decrement
+let j = 10;
+let g = --j;
+let o = --j;
+console.log(j, o, g);
